425a250dfa52cc1862d27ae36f36d1d2ea2ca5a5
Iman <imankhorshidi72@gmail.com>
2022-03-16 02:21:39 +0330

    Add: Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { Routes, Route } from "react-router-dom";
 
 import { createGlobalStyle } from "styled-components";
 import { shopData } from "./data/shopData";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
@@ -21,26 +23,28 @@ export default App;
 
 const GlobalStyles = createGlobalStyle`
 
-
 :root{
   --color-black:#000;
   --color-white:#fff;
-  --color-grey-1:#4a4a4a;
+  --color-grey-1:#343a40;
+  --color-grey-2:#868e96;
+  --color-grey-3:#212529;
 }
 
 *,*::after,*::before{
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  color: var(--color-grey-1);
 }
 
 body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+  font-family:  "Nunito", -apple-system, BlinkMacSystemFont, "Segoe UI", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: var(--color-grey-1);
+ 
   padding: 20px 60px;
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="links">
+        <NavLink className="link" to="/shop">
+          SHOP
+        </NavLink>
+        <NavLink className="link" to="/contact">
+          CONTACT
+        </NavLink>
+      </div>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
+
+const HeaderWrapper = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 25px;
+
+  .links {
+    display: flex;
+    align-items: center;
+    gap: 32px;
+
+    .link {
+      color: var(--color-grey-2);
+      transition: all 0.3s;
+
+      &:hover {
+        color: var(--color-grey-3);
+      }
+    }
+
+    .active {
+      color: var(--color-grey-3);
+    }
+  }
+`;
